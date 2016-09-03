4cee5050b297314fe524181b012da33cab3500a5
Jérôme Vizcaino <jerome.vizcaino@gmail.com>
2016-09-03 19:27:50 +0200

    main: add support to select exporter

--- goconso.go
+++ goconso.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-
+	"fmt"
 	"goconso/exporters"
 	_ "goconso/exporters/hphc"
 	"goconso/teleinfo"
@@ -12,27 +12,35 @@ import (
 
 func main() {
 	var serialDevice string
+	var exporterName string
 
 	flag.StringVar(&serialDevice, "device", "/dev/ttyUSB0", "Serial port to read frames from")
+	flag.StringVar(&exporterName, "export", "", fmt.Sprintf("Exporter module name. Available: %v", exporters.List()))
 	flag.Parse()
 
+	newExporter := exporters.FindByName(exporterName)
+	if newExporter == nil {
+		glog.Exitf("Unknown exporter '%s', valid choices are %v\n", exporterName, exporters.List())
+	}
+	exporter, err := newExporter()
+	if err != nil {
+		glog.Exitf("Error creating exporter '%s' (%s)\n", exporterName, err)
+	}
 	port, err := teleinfo.OpenPort(serialDevice)
 	if err != nil {
 		glog.Exitf("Error: %s", err)
 	}
 	defer port.Close()
 	reader := teleinfo.NewReader(port)
-	newExporter := exporters.FindByName("hphc.json")
-	exporter, err := newExporter()
-	if err != nil {
-		glog.Exitf("Error creating exporter 'hphc.json' (%s)\n", err)
-	}
 
 	for {
 		frame, err := reader.ReadFrame()
 		if err != nil {
 			glog.Exitf("Error reading frame from '%s' (%s)\n", serialDevice, err)
 		}
-		exporter.ExportFrame(frame)
+		err = exporter.ExportFrame(frame)
+		if err != nil {
+			glog.Errorf("Error exporting frame (%s)\n", err)
+		}
 	}
 }
